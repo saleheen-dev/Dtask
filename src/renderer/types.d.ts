f249b3aea78139952e1fc65a888ed0ea9f9f2fff
Saleheen Noor <saleheen.noor@gmail.com>
2026-05-21 15:27:36 +0500

    feat: add type declarations for image modules (.png, .jpg, .svg)

--- src/renderer/types.d.ts
+++ src/renderer/types.d.ts
@@ -1,3 +1,20 @@
+/// <reference types="vite/client" />
+
+declare module "*.png" {
+  const src: string
+  export default src
+}
+
+declare module "*.jpg" {
+  const src: string
+  export default src
+}
+
+declare module "*.svg" {
+  const src: string
+  export default src
+}
+
 import type { ProcessInfo, KillResult } from '../common/types'
 
 declare global {
